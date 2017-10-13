0d3f7c5070628abd593673dbc9c6af679d9653b6
amraymankamal <amrayman1996@yahoo.com>
2017-10-13 17:45:24 +0100

    amr2

--- src/library/Book.java
+++ src/library/Book.java
@@ -1,5 +1,12 @@
 
+
+
 package library;
+
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import javafx.scene.image.Image;
 
 /**
  *
@@ -10,17 +17,29 @@ public class Book {
     public String Author;
     public String Description;
     public String ISBN;
+    public String Publisher;
     public String Genre;
-    public int quantity;
+    public int quantity = 5;
+    public String cover;
     public float rating;
+    public Image image;
     
     
-    public Book(String t , String a , String d , String i ,String g){
+    public Book (String t , String a , String d , String i ,String g,String p, String image){
+        
+        this.cover = image;
+         this.image = new Image("/library/"+image);
         this.Title = t;
         this.Author = a;
         this.Description = d;
         this.ISBN = i;
         this.Genre = g;
+        this.Publisher = p;
+    }
+    public void load() throws FileNotFoundException {  
+        this.image = new Image(new FileInputStream("/library/"+image));
+     
+    
     }
     
     public void calcRating(){
