055a6fee9b5ba56f878812de17d21d162e52a7bc
Yasmin Essam <yasmin.ali146@gmail.com>
2017-11-13 20:46:07 +0200

    :+1:

--- src/library/HistoryController.java
+++ src/library/HistoryController.java
@@ -5,9 +5,13 @@
  */
 package library;
 
+import com.jfoenix.controls.JFXTextField;
 import insidefx.undecorator.Undecorator;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -15,9 +19,12 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.Region;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  * FXML Controller class
@@ -25,7 +32,98 @@ import javafx.stage.Stage;
  * @author Farida Abouish
  */
 public class HistoryController implements Initializable {
+ @FXML
+    private TableView<info> result;
+    @FXML
+    private JFXTextField input;
+    @FXML
+    private JFXTextField bookISBN;
+    @FXML
+    private TableColumn<info, String> isbn,title,genre;
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        title.setCellValueFactory(cellData -> cellData.getValue().getTitle());
+        genre.setCellValueFactory(cellData -> cellData.getValue().getGenre());
+        isbn.setCellValueFactory(cellData -> cellData.getValue().getISBN());
+       fill();
+       result.getSelectionModel().selectedItemProperty().addListener((obs, oldSelection, newSelection) -> {
+                        Stage stage=new Stage();
+           FXMLLoader loader = new FXMLLoader();
+           loader.setLocation(getClass().getResource("BookView.fxml"));
+         try {
+             loader.load();       
+        } catch(Exception e) {
+           e.printStackTrace();
+          }
+         
+             BookViewController d = loader.getController();
+                d.getBook(newSelection.getISBN().getValue());
+                //System.out.println(loginID.getText());
+         Parent root1 = loader.getRoot();  
+         stage.initStyle(StageStyle.TRANSPARENT);
+            Undecorator undecorator = new Undecorator(stage, (Region) root1);
+ 
+// Default theme
+ undecorator.getStylesheets().add("skin/undecorator.css");
+ Scene scene1 = new Scene(undecorator);
+ scene1.setFill(Color.TRANSPARENT);
+stage.setScene(scene1);
+stage.show();
+        stage.setResizable(false);
 
+       
+});
+    }   
+    private void fill(){
+        ObservableList <info> content=FXCollections.observableArrayList();
+        ArrayList<Book> blist=Library.bookList;
+        for(int i=0;i<blist.size();i++){
+            Book tmp=blist.get(i);
+            content.add(new info(tmp.ISBN,tmp.Title,tmp.Genre));
+        }
+        result.setItems(content);
+     
+    }
+    @FXML
+    public void searchBook(){
+        input.textProperty().addListener((observable, oldText, newText)->{ // lambda style
+        if(newText.isEmpty())fill();
+                else{
+        Library searchable=new Library();
+        ObservableList <info> returned = searchable.search(newText);
+        result.setItems(returned);
+        }
+        });;  
+
+}
+    
+        @FXML
+       private void openBook(ActionEvent event) {
+           FXMLLoader loader = new FXMLLoader();
+           loader.setLocation(getClass().getResource("BookView.fxml"));
+         try {
+             loader.load();       
+        } catch(Exception e) {
+           e.printStackTrace();
+          }
+         
+             BookViewController d = loader.getController();
+             Stage stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+                d.getBook(bookISBN.getText());
+                //System.out.println(loginID.getText());
+         Parent root1 = loader.getRoot();            
+            Undecorator undecorator = new Undecorator(stage, (Region) root1);
+ 
+// Default theme
+ undecorator.getStylesheets().add("skin/undecorator.css");
+ Scene scene1 = new Scene(undecorator);
+ scene1.setFill(Color.TRANSPARENT);
+stage.setScene(scene1);
+stage.show();
+        stage.setResizable(false);
+
+       
+    }
    @FXML
     private void back(ActionEvent event){
         FXMLLoader loader = new FXMLLoader();
@@ -52,9 +150,8 @@ stage.show();
         stage.setResizable(false);
 
     }
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+    
+   
+      
     
 }
